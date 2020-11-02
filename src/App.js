fe1c9a75ecae46de9e9d44c5ebcf20a0d40dff19
onionsmit <minhho.technology@gmail.com>
2020-11-02 17:33:06 +0700

    finish component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import TaskForm from './component/TaskForm'
+import TaskList from './component/TaskList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+            <div className="col-md-4">
+            </div>
+            <div className="col-md-4">
+                <h1>Quản lý công việc</h1>
+            </div>
+            <div className="col-md-4">
+            </div>
+        </div>
+        <div className="row">
+            <TaskForm></TaskForm>
+            <TaskList></TaskList>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
